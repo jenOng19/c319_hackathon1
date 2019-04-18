a3c496e9994c143c2a98c0c41e5a59e8bf64f6fe
jenOng19 <jennifer.sy.ong@gmail.com>
2019-04-17 17:24:19 -0700

    render cards and spices dynamically

--- assets/Components/Card/SpiceUpgradeCard.js
+++ assets/Components/Card/SpiceUpgradeCard.js
@@ -12,11 +12,23 @@ class SpiceUpgradeCard extends Card {
 
     render () {
         const cardElement = $('<div>')
-                                .addClass('card spice-upgrade-card')
+                                .addClass('card merchant-card upgrade-card')
                                 .on({
                                     'click': this.cardClickHandler
                                 })
-                                .text('SpiceUpgradeCard');
+
+        const cardFunctionElement = $('<div>').addClass('card-function');
+        const arrow=$('<div>').addClass('upgrade-arrow arrow').html('<span>&uarr;</span>');
+        const upgradeSpiceContainer=$('<div>').addClass('upgrade-spice');
+
+        for(var upgrade=0; upgrade<this._upgradeTimes;upgrade++){
+            const spiceElement=$('<div>').addClass('card-spice gray');
+            upgradeSpiceContainer.append(spiceElement);
+        }
+
+        cardFunctionElement.append(arrow, upgradeSpiceContainer);
+        cardElement.append(cardFunctionElement);
+
         return cardElement;
     
     }
