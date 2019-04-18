7666e9f1a1d05f1ab814b993208aa6c0873c70d3
Paulina C <paulinachee.is@gmail.com>
2019-04-17 17:29:09 -0700

    point card update and html getting rid of extras

--- assets/Components/Card/PointCard.js
+++ assets/Components/Card/PointCard.js
@@ -22,8 +22,35 @@ class PointCard extends Card {
                                 .addClass('card point-card')
                                 .on({
                                     'click': this.cardClickHandler
-                                })
-                                .text('PointCard');
+                                });
+
+        const cardPointDisplayElement = $("<div>").addClass("points-display").text("15");
+        const cardPriceElement = $("<div>").addClass("price flex flex-center");
+
+        for (let spice of this._spiceList) {
+            const spiceElement = $('<div>').addClass('card-spice ' + spice);
+            cardPriceElement.append(spiceElement);
+        }
+
+        cardElement.append(cardPointDisplayElement, cardPriceElement);
+
         return cardElement;
     }
 }
+
+
+
+const cardElement = $('<div>')
+                                .addClass('card merchant-card obtain-card')
+                                .on({
+                                    'click': this.cardClickHandler
+                                });
+        const cardFunctionElement = $('<div>').addClass('card-function');
+        const obtainSpiceElement = $('<div>').addClass('obtain-spice');
+
+        for (let spice of this._spiceList) {
+            const spiceElement = $('<div>').addClass('card-spice ' + spice);
+            obtainSpiceElement.append(spiceElement);
+        }
+        cardFunctionElement.append(obtainSpiceElement);
+        cardElement.append(cardFunctionElement);
